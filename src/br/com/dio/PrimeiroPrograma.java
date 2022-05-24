0c44aeadac0df25f477a2d944bc7f0b57adb9e73
Thainara Carvalho <thai.cds1@gmail.com>
2022-05-24 14:09:22 -0400

    Criando class livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -7,10 +7,44 @@ public class PrimeiroPrograma {
         Gato gato = new Gato();
         System.out.println(gato);
 
+        Livro livro1 = new Livro("Os sete maridos de Evelynn Hugo", 300);
+        System.out.println(livro1);
 
+    }
+}
+class Livro{
+    private String nome;
+    private Integer numPaginas;
 
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
 
+    public String getNome() {
+        return nome;
+    }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+}
 
 
 
@@ -20,5 +54,3 @@ public class PrimeiroPrograma {
 /*        ctrl + shift + / = comentar bloco */
 //        ctrl + y =  deletar linha
 //        ctrl + inset = menu
-    }
-}

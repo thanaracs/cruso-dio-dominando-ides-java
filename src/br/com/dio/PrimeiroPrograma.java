2db4a90e55957cb025dfff7b1154ae8f0115cdec
Thainara Carvalho <thai.cds1@gmail.com>
2022-05-24 14:02:02 -0400

    First commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,24 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+
+
+
+
+
+
+
+//        ctrl + alt + l = arrumar linha
+//        ctrl + d = duplicar linha
+//        ctrl +  / = comentar
+/*        ctrl + shift + / = comentar bloco */
+//        ctrl + y =  deletar linha
+//        ctrl + inset = menu
+    }
+}
